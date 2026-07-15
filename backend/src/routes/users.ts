0363dcb3c5cc2e1e713c1637b4b6352a5dabeef2
Xuan4781 <Xuan4781@users.noreply.github.com>
2026-07-14 21:48:47 -0400

    Add GET users route

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -1,47 +1,22 @@
 import { Router } from 'express';
 import bcrypt from 'bcryptjs';
 import { prisma } from '../prisma';
+import { requireAuth, AuthRequest } from '../middleware/auth';
 
 const router = Router();
 
-// POST /users
-router.post('/', async (req, res) => {
-  try {
-    const { email, password, name, role } = req.body;
+router.get("/me", requireAuth, async (req: AuthRequest, res) => {
+  const user = await prisma.user.findUnique({
+    where: {id: req.userId},
+    select: {id: true, email:true, name: true, role: true, createdAt: true},
+  })
 
-    if (!email || !password || !name) {
-      return res.status(400).json({ error: 'thee email, password, and name is required' });
-    }
-
-    const existing = await prisma.user.findUnique({ where: { email } });
-    if (existing) {
-      return res.status(409).json({ error: 'THIS user with that email already existss' });
-    }
-
-    const passwordHash = await bcrypt.hash(password, 10);
-
-    const user = await prisma.user.create({
-      data: {
-        email,
-        passwordHash,
-        name,
-        role: role === 'artist' ? 'artist' : 'buyer',
-      },
-      select: {
-        id: true,
-        email: true,
-        name: true,
-        role: true,
-        createdAt: true,
-      },
-    });
-
-    res.status(201).json(user);
-  } catch (err) {
-    console.error('Error creating the user:', err);
-    res.status(500).json({ error: 'Something went wrong with creting user' });
+  if(!user){
+    return res.status(404).json({error: "User not found"});
   }
-});
+
+  res.json(user);
+})
 
 // GET /users/:id 
 router.get('/:id', async (req, res) => {
